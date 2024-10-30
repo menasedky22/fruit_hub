98139d5c92f1364070a972d22674b2311a6ec948
mena <menasedky22@gmail.com>
2024-10-30 17:03:36 +0300

    dot indicator logic and add text style

--- lib/features/on_boarding/presentation/views/widgets/on_boarding_page_view.dart
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_page_view.dart
@@ -3,28 +3,39 @@ import 'package:fruit_hub/core/utils/app_images.dart';
 import 'package:fruit_hub/features/on_boarding/presentation/views/widgets/page_view_item.dart';
 
 class OnBoardingPageView extends StatelessWidget {
-  const OnBoardingPageView({super.key});
-
+  const OnBoardingPageView({super.key, required this.pageController});
+  final PageController pageController;
   @override
   Widget build(BuildContext context) {
     return PageView(
-      children: const [
+      controller: pageController,
+      children: [
         PageViewItem(
+            isVisible: (pageController.hasClients
+                    ? pageController.page!.round()
+                    : 0) ==
+                0,
             image: Assets.imagesPageviewitem1Image,
             backgroundImage: Assets.imagesPageviewitem1Backgroundimage,
             subtitle:
                 'اكتشف تجربة تسوق فريدة مع FruitHUB. استكشف مجموعتنا الواسعة من الفواكه الطازجة الممتازة واحصل على أفضل العروض والجودة العالية.',
-            title: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Text('مرحبًا بك في'),
-              Text('Fruit'),
-              Text('HUB'),
-            ])),
+            title: const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('مرحبًا بك في'),
+                  Text('Fruit'),
+                  Text('HUB'),
+                ])),
         PageViewItem(
+            isVisible: (pageController.hasClients
+                    ? pageController.page!.round()
+                    : 0) !=
+                0,
             image: Assets.imagesPageviewitem2Image,
             backgroundImage: Assets.imagesPageviewitem2Backgroundimage,
             subtitle:
                 'نقدم لك أفضل الفواكه المختارة بعناية. اطلع على التفاصيل والصور والتقييمات لتتأكد من اختيار الفاكهة المثالية',
-            title: Text(
+            title: const Text(
               'ابحث وتسوق',
               textAlign: TextAlign.center,
               style: TextStyle(
--- lib/features/on_boarding/presentation/views/widgets/page_view_item.dart
+++ lib/features/on_boarding/presentation/views/widgets/page_view_item.dart
@@ -7,12 +7,13 @@ class PageViewItem extends StatelessWidget {
       required this.image,
       required this.backgroundImage,
       required this.subtitle,
-      required this.title});
+      required this.title,
+      required this.isVisible});
 
   final String image, backgroundImage;
   final String subtitle;
   final Widget title;
-
+  final bool isVisible;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -36,9 +37,12 @@ class PageViewItem extends StatelessWidget {
                   image,
                 ),
               ),
-              const Padding(
-                padding: EdgeInsets.all(16),
-                child: Text('تخط'),
+              Visibility(
+                visible: isVisible,
+                child: const Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Text('تخط'),
+                ),
               )
             ],
           ),
